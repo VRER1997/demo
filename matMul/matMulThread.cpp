b23551327858d56e0562e4731e8f50f387dbfd88
vrer1997 <1216778435@qq.com>
2020-03-06 18:25:33 +0800

    matThread

--- matMul/matMulThread.cpp
+++ matMul/matMulThread.cpp
@@ -1,66 +1,97 @@
-// CPP Program to multiply two matrix using pthreads
 #include <bits/stdc++.h>
 using namespace std;
 
-// maximum size of matrix
-#define N 100
+#define N 128
 
-int *matA, *matB, *matC;;
+int *matA, *matB, *matC, *matCC;
 int step_i = 0;
+mutex data_mutex;
 
-
-void* multi(void* arg)
-{
+void* task2(void* arg) {
+    unique_lock<std::mutex> lock(data_mutex);
     int core = step_i++;
     for (int i = 0; i < N; ++i)
         for (int k = 0; k < N; ++k)
             matC[i*N + core] += matA[i*N + k] * matB[k*N + core];
+    return 0;
 }
 
-void test() {
-
+void matMul_cpu2() {
+    pthread_t threads[N];
+    for (int i = 0; i < N; i++) {
+        int* p;
+        pthread_create(&threads[i], NULL, task2, (void*)(p));
+    }
+    for (int i = 0; i < N; i++) {
+        pthread_join(threads[i], NULL);
+    }
 }
 
-void check(int *d_z) {
+int step_ij = 0;
+mutex data_mutex3;
+void* task3(void* arg) {
+    unique_lock<std::mutex> lock(data_mutex3);
+    int core = step_ij++;
+    int i = core / N;
+    int j = core % N;
+    for (int k = 0; k < N; ++k) {
+        matCC[i*N + j] += matA[i*N + k] * matB[k*N + j];
+    }
+    return 0;
+}
+
+void matMul_cpu3() {
+    pthread_t threads[N * N];
+    for (int i = 0; i < N * N; i++) {
+        int* p;
+        pthread_create(&threads[i], NULL, task3, (void*)(p));
+    }
+    for (int i = 0; i < N * N; i++) {
+        pthread_join(threads[i], NULL);
+    }
+}
+
+bool check(int *d_z) {
     for (int i = 0; i < N*N; i++) {
         if (d_z[i] != N) {
             cout << "error " << d_z[i] << endl;
-            return;
+            return false;
         }
     }
-    cout << "right" << endl;
+    return true;
 }
 
-int main()
-{
+int main() {
     int M = sizeof(int) * N * N;
     matA = (int*)malloc(M);
     matB = (int*)malloc(M);
     matC = (int*)malloc(M);
+    matCC = (int*)malloc(M);
 
     for (int i = 0; i < N * N; i++) {
         matA[i] = 1;
         matB[i] = 1;
     }
+    auto start = chrono::steady_clock::now();
+    matMul_cpu2();
+    auto end = chrono::steady_clock::now();
+    auto cputime2 = chrono::duration_cast<chrono::milliseconds> (end-start).count();
+    bool ret1 = check(matC);
 
-    // declaring four threads
-    pthread_t threads[N];
+    start = chrono::steady_clock::now();
+    if(N < 129) matMul_cpu3();
+    end = chrono::steady_clock::now();
+    auto cputime3 = chrono::duration_cast<chrono::milliseconds> (end-start).count();
+    bool ret2 = true;
+    if(N < 129) ret2 = check(matCC);
 
-    // Creating four threads, each evaluating its own part
-    for (int i = 0; i < N; i++) {
-        int* p;
-        pthread_create(&threads[i], NULL, multi, (void*)(p));
-    }
+    printf("Num: %d   Reuslt: %s   CPUTime2: %ld ms   CPUTime3: %ld ms \n",
+            N, ret1 && ret2 ? "Right!!!" : "Wrong...", cputime2, cputime3);
 
-    for (int i = 0; i < N; i++)
-        pthread_join(threads[i], NULL);
-
-//    for (int i = 0; i < N; i++) {
-//        for (int j = 0; j < N; j++)
-//            cout << matC[i*N + j] << " ";
-//        cout << endl;
-//    }
-    check(matC);
+    free(matA);
+    free(matB);
+    free(matC);
+    free(matCC);
 
     return 0;
 }
